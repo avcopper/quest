868118bdd9b7f4cedc497d7382f4910aa150caf5
andrew <andreycopper@gmail.com>
2023-10-25 18:25:00 +0600

    constants

--- src/main/java/com/javarush/quest/cooper/constants/QuestConstants.java
+++ src/main/java/com/javarush/quest/cooper/constants/QuestConstants.java
@@ -5,10 +5,11 @@ public class QuestConstants {
     public static final String LEVEL = "level";
     public static final String ANSWER = "answer";
     public static final String NAME = "name";
+    public static final String ERROR = "error";
     public static final String DEFAULT_NAME = "Nameless Clown";
     public static final String QUEST_FILE = "C:\\DEVELOP\\JAVA\\quest\\quest.json";
 
     public static final String UNSUPPORTED_VALUE = "Unsupported value";
     public static final String SESSION_IS_BROKEN = "Session is broken, try one more time";
-    public static final String SETTINGS_IS_BROKEN = "Settings file is broken";
+    public static final String QUEST_FILE_IS_LOST = "Quest file is lost";
 }
